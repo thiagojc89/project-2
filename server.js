378f27f80c0115284851373f40df2065901f7867
Thiago Julian Cavalcante <thiagojc89@gmail.com>
2020-10-29 10:05:03 -0400

    express server basic set up done

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+
+app.get('/', (req, res)=>{
+    res.send("Ready to rock!!!")
+})
+
+
+
+
+app.listen(3000,()=>{
+    console.log("Server is listening!!!")
+})
