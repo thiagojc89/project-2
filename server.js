266379dabd1ea84ad77f46577a8dc63ad7aee492
Thiago Julian Cavalcante <thiagojc89@gmail.com>
2020-10-29 12:20:16 -0400

    added port

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const PORT = process.env.PORT || 3000
 
 
 app.get('/', (req, res)=>{
@@ -9,6 +10,6 @@ app.get('/', (req, res)=>{
 
 
 
-app.listen(3000,()=>{
+app.listen(PORT,()=>{
     console.log("Server is listening!!!")
 })
